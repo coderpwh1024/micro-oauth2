9038d902fd538153eedd89a3b4708d83c663ea2a
coderpwh <pwhfbc@outlook.com>
2024-06-27 22:26:09 +0800

    Add files via upload

---
+++ micro-oauth2-api/src/main/java/com/coderpwh/Oauth2ApiApplication.java
@@ -0,0 +1,19 @@
+package com.coderpwh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ *
+ * @author coderpwh
+ */
+@EnableDiscoveryClient
+@SpringBootApplication
+public class Oauth2ApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Oauth2ApiApplication.class, args);
+    }
+
+}
