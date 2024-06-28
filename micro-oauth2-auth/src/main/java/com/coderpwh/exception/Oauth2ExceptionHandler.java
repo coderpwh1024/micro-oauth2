ec4269338f825bbc9ae413faf706b9fd9d65bff8
pengwenhao <pengwenhao@aliyun.com>
2024-06-28 15:31:52 +0800

    feat:init exception

--- micro-oauth2-auth/src/main/java/com/coderpwh/exception/Oauth2ExceptionHandler.java
+++ micro-oauth2-auth/src/main/java/com/coderpwh/exception/Oauth2ExceptionHandler.java
@@ -1,6 +1,9 @@
 package com.coderpwh.exception;
 
+import com.coderpwh.api.CommonResult;
+import org.springframework.security.oauth2.common.exceptions.OAuth2Exception;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * @author coderpwh
@@ -8,6 +11,9 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 @ControllerAdvice
 public class Oauth2ExceptionHandler {
 
-
+    @ResponseBody
+    public CommonResult handleOauth2(OAuth2Exception e) {
+        return CommonResult.failed(e.getMessage());
+    }
 
 }
