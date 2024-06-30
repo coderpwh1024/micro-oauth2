bc6a402f51fde91141d37adae7aff294fdae04be
pengwenhao <pengwenhao@aliyun.com>
2024-07-01 06:25:31 +0800

    feat:init constan

---
+++ micro-oauth2-gateway/src/main/java/com/coderpwh/constant/AuthConstant.java
@@ -0,0 +1,10 @@
+package com.coderpwh.constant;
+
+/**
+ * @author coderpwh
+ */
+public class AuthConstant {
+    public static final String AUTHORITY_PREFIX = "ROLE_";
+
+    public static final String AUTHORITY_CLAIM_NAME = "authorities";
+}
---
+++ micro-oauth2-gateway/src/main/java/com/coderpwh/constant/RedisConstant.java
@@ -0,0 +1,9 @@
+package com.coderpwh.constant;
+
+/**
+ * @author coderpwh
+ */
+public class RedisConstant {
+
+    public static final String RESOURCE_ROLES_MAP = "AUTH:RESOURCE_ROLES_MAP";
+}
