58759952332c8899f20f31eeb63722d43369e789
pengwenhao <pengwenhao@aliyun.com>
2024-06-30 22:22:16 +0800

    feat:init AuthController

---
+++ micro-oauth2-auth/src/main/java/com/coderpwh/controller/AuthController.java
@@ -0,0 +1,46 @@
+package com.coderpwh.controller;
+
+import com.coderpwh.api.CommonResult;
+import com.coderpwh.domain.Oauth2TokenDTO;
+import org.springframework.security.oauth2.common.OAuth2AccessToken;
+import org.springframework.security.oauth2.provider.endpoint.TokenEndpoint;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.security.Principal;
+import java.util.Map;
+
+/**
+ * @author coderpwh
+ */
+@RestController
+@RequestMapping(value = "oauth")
+public class AuthController {
+
+
+    @Resource
+    private TokenEndpoint tokenEndpoint;
+
+
+    /***
+     * 授权登录
+     * @return
+     */
+    @RequestMapping(value = "/token", method = RequestMethod.POST)
+    public CommonResult<Oauth2TokenDTO> postAccessToken(Principal principal, Map<String, String> parameters) throws HttpRequestMethodNotSupportedException {
+        OAuth2AccessToken oAuth2AccessToken = tokenEndpoint.postAccessToken(principal, parameters).getBody();
+
+        Oauth2TokenDTO oauth2TokenDTO = Oauth2TokenDTO.builder()
+                .token(oAuth2AccessToken.getValue())
+                .refreshToken(oAuth2AccessToken.getRefreshToken().getValue())
+                .expiresIn(oAuth2AccessToken.getExpiresIn())
+                .tokenHead("Bearer ").build();
+        return CommonResult.success(oauth2TokenDTO);
+    }
+
+
+}
+
