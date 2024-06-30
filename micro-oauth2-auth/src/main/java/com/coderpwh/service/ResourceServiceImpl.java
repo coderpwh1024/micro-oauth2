5830e6a8cb4cc3a2965be3635fc40182eb02220b
pengwenhao <pengwenhao@aliyun.com>
2024-06-30 21:57:07 +0800

    fix:update ResourceServiceImpl

--- micro-oauth2-auth/src/main/java/com/coderpwh/service/ResourceServiceImpl.java
+++ micro-oauth2-auth/src/main/java/com/coderpwh/service/ResourceServiceImpl.java
@@ -1,12 +1,16 @@
 package com.coderpwh.service;
 
+import cn.hutool.core.collection.CollUtil;
+import com.coderpwh.constant.RedisConstant;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.PostConstruct;
 import javax.annotation.Resource;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
+import java.util.TreeMap;
 
 /**
  * @author coderpwh
@@ -21,6 +25,15 @@ public class ResourceServiceImpl {
 
     @Resource
     private RedisTemplate<String, Object> redisTemplate;
+
+    @PostConstruct
+    public  void initData(){
+
+        resourceRolesMap =   new TreeMap<>();
+        resourceRolesMap.put("/api/hello", CollUtil.toList("ADMIN"));
+        resourceRolesMap.put("/api/user/currentUser", CollUtil.toList("ADMIN", "TEST"));
+        redisTemplate.opsForHash().putAll(RedisConstant.RESOURCE_ROLES_MAP, resourceRolesMap);
+    }
 
 
 
