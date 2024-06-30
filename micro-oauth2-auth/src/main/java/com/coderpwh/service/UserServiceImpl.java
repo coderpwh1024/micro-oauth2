136cd0d8c4ae6b84e72ee6673f044ee08734dde0
pengwenhao <pengwenhao@aliyun.com>
2024-06-30 16:56:37 +0800

    fix:update UserServiceImpl

--- micro-oauth2-auth/src/main/java/com/coderpwh/service/UserServiceImpl.java
+++ micro-oauth2-auth/src/main/java/com/coderpwh/service/UserServiceImpl.java
@@ -1,8 +1,13 @@
 package com.coderpwh.service;
 
 import cn.hutool.core.collection.CollUtil;
+import com.coderpwh.constant.MessageConstant;
+import com.coderpwh.domain.SecurityUser;
 import com.coderpwh.domain.UserDTO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.CredentialsExpiredException;
+import org.springframework.security.authentication.DisabledException;
+import org.springframework.security.authentication.LockedException;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -41,12 +46,22 @@ public class UserServiceImpl implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        List<UserDTO> findUserList = userList.stream().filter(item -> item.getUsername().equals(username)).collect(Collectors.toList());
 
-        List<UserDTO> findUserList = userList.stream().filter(item->item.getUsername().equals(username)).collect(Collectors.toList());
+        if (CollUtil.isEmpty(findUserList)) {
+            throw new UsernameNotFoundException(MessageConstant.USERNAME_PASSWORD_ERROR);
+        }
 
+        SecurityUser securityUser = new SecurityUser(findUserList.get(0));
 
-
-        return null;
+        if (!securityUser.isEnabled()) {
+            throw new DisabledException(MessageConstant.ACCOUNT_DISABLED);
+        } else if (!securityUser.isAccountNonLocked()) {
+            throw new LockedException(MessageConstant.ACCOUNT_LOCKED);
+        } else if (!securityUser.isCredentialsNonExpired()) {
+            throw new CredentialsExpiredException(MessageConstant.CREDENTIALS_EXPIRED);
+        }
+        return securityUser;
     }
 
 
