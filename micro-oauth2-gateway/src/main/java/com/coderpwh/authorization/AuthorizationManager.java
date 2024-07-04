3801a2519ed1a6c7ca939cba15d914e72aae455c
pengwenhao <pengwenhao@aliyun.com>
2024-07-04 15:15:35 +0800

    feat:init AuthorizationManager

---
+++ micro-oauth2-gateway/src/main/java/com/coderpwh/authorization/AuthorizationManager.java
@@ -0,0 +1,54 @@
+package com.coderpwh.authorization;
+
+import cn.hutool.core.convert.Convert;
+import com.coderpwh.constant.AuthConstant;
+import com.coderpwh.constant.RedisConstant;
+import org.checkerframework.checker.units.qual.C;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.security.authorization.AuthorizationDecision;
+import org.springframework.security.authorization.ReactiveAuthorizationManager;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.web.server.authorization.AuthorizationContext;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
+
+import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * @author coderpwh
+ */
+@Component
+public class AuthorizationManager implements ReactiveAuthorizationManager<AuthorizationContext> {
+
+
+    @Autowired
+    private RedisTemplate<String, Object> redisTemplate;
+
+
+    @Override
+    public Mono<AuthorizationDecision> check(Mono<Authentication> authentication, AuthorizationContext object) {
+
+
+        URI uri = object.getExchange().getRequest().getURI();
+        Object obj = redisTemplate.opsForHash().get(RedisConstant.RESOURCE_ROLES_MAP, uri.getPath());
+
+        List<String> authorities = Convert.toList(String.class, obj);
+
+        authorities  = authorities.stream().map(i->i= AuthConstant.AUTHORITY_PREFIX+i).collect(Collectors.toList());
+
+        return  authentication
+                .filter(Authentication::isAuthenticated)
+                .flatMapIterable(Authentication::getAuthorities)
+                .map(GrantedAuthority::getAuthority)
+                .any(authorities::contains)
+                .map(AuthorizationDecision::new)
+                .defaultIfEmpty(new AuthorizationDecision(false));
+
+    }
+
+
+}
