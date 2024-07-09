f15dad19f62c4e7c58e43312bb71a6a045c3a15a
pengwenhao <pengwenhao@aliyun.com>
2024-07-09 13:01:15 +0800

    fix:update ResourceServerConfig

--- micro-oauth2-gateway/src/main/java/com/coderpwh/config/ResourceServerConfig.java
+++ micro-oauth2-gateway/src/main/java/com/coderpwh/config/ResourceServerConfig.java
@@ -1,9 +1,20 @@
 package com.coderpwh.config;
 
+import com.coderpwh.component.RestAuthenticationEntryPoint;
+import com.coderpwh.component.RestfulAccessDeniedHandler;
+import com.coderpwh.constant.AuthConstant;
 import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
+import org.springframework.security.oauth2.server.resource.authentication.ReactiveJwtAuthenticationConverterAdapter;
+import reactor.core.publisher.Mono;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.core.convert.converter.Converter;
 
 /**
  * @author coderpwh
@@ -16,7 +27,21 @@ public class ResourceServerConfig {
 
     private final AuthenticationManager authenticationManager;
 
-    private  final  IgnoreUrlsConfig ignoreUrlsConfig;
+    private final IgnoreUrlsConfig ignoreUrlsConfig;
 
+    private final RestfulAccessDeniedHandler restfulAccessDeniedHandler;
+
+    private final RestAuthenticationEntryPoint restAuthenticationEntryPoint;
+
+
+    @Bean
+    public Converter<Jwt, ? extends Mono<? extends AbstractAuthenticationToken>> jwtAuthenticationConverter() {
+        JwtGrantedAuthoritiesConverter jwtGrantedAuthoritiesConverter = new JwtGrantedAuthoritiesConverter();
+        jwtGrantedAuthoritiesConverter.setAuthorityPrefix(AuthConstant.AUTHORITY_PREFIX);
+        jwtGrantedAuthoritiesConverter.setAuthoritiesClaimName(AuthConstant.AUTHORITY_CLAIM_NAME);
+        JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
+        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(jwtGrantedAuthoritiesConverter);
+        return new ReactiveJwtAuthenticationConverterAdapter(jwtAuthenticationConverter);
+    }
 
 }
