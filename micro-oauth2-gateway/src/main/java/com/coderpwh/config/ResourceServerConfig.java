98e68e5b8cd3c09ffcf46fdc0c87cb7703bd6fbc
pengwenhao <pengwenhao@aliyun.com>
2024-07-09 13:07:21 +0800

    fix:update ResourceServerConfig

--- micro-oauth2-gateway/src/main/java/com/coderpwh/config/ResourceServerConfig.java
+++ micro-oauth2-gateway/src/main/java/com/coderpwh/config/ResourceServerConfig.java
@@ -1,17 +1,22 @@
 package com.coderpwh.config;
 
+import cn.hutool.core.util.ArrayUtil;
+import com.coderpwh.authorization.AuthorizationManager;
 import com.coderpwh.component.RestAuthenticationEntryPoint;
 import com.coderpwh.component.RestfulAccessDeniedHandler;
 import com.coderpwh.constant.AuthConstant;
+import com.coderpwh.filter.IgnoreUrlsRemoveJwtFilter;
 import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AbstractAuthenticationToken;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.web.server.SecurityWebFiltersOrder;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
 import org.springframework.security.oauth2.server.resource.authentication.ReactiveJwtAuthenticationConverterAdapter;
+import org.springframework.security.web.server.SecurityWebFilterChain;
 import reactor.core.publisher.Mono;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.core.convert.converter.Converter;
@@ -25,13 +30,42 @@ import org.springframework.core.convert.converter.Converter;
 public class ResourceServerConfig {
 
 
-    private final AuthenticationManager authenticationManager;
+    private final AuthorizationManager authorizationManager;
 
     private final IgnoreUrlsConfig ignoreUrlsConfig;
 
     private final RestfulAccessDeniedHandler restfulAccessDeniedHandler;
 
     private final RestAuthenticationEntryPoint restAuthenticationEntryPoint;
+
+    private final IgnoreUrlsRemoveJwtFilter ignoreUrlsRemoveJwtFilter;
+
+
+    @Bean
+    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+
+        http.oauth2ResourceServer().jwt()
+                .jwtAuthenticationConverter(jwtAuthenticationConverter());
+
+        http.oauth2ResourceServer().authenticationEntryPoint(restAuthenticationEntryPoint);
+
+        //对白名单路径，直接移除JWT请求头
+        http.addFilterBefore(ignoreUrlsRemoveJwtFilter, SecurityWebFiltersOrder.AUTHENTICATION);
+
+        http.authorizeExchange()
+                //白名单配置
+                .pathMatchers(ArrayUtil.toArray(ignoreUrlsConfig.getUrls(), String.class)).permitAll()
+                //鉴权管理器配置
+                .anyExchange().access(authorizationManager)
+                //处理未授权
+                .and().exceptionHandling()
+                .accessDeniedHandler(restfulAccessDeniedHandler)
+                //处理未认证
+                .authenticationEntryPoint(restAuthenticationEntryPoint)
+                .and().csrf().disable();
+        return http.build();
+
+    }
 
 
     @Bean
