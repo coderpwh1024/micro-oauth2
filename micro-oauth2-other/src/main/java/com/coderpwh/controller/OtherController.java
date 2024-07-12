1e4813abea07459c4c678002bd0e2d7402fda4a1
pengwenhao <pengwenhao@aliyun.com>
2024-07-12 16:32:37 +0800

    feat:init OtherController

---
+++ micro-oauth2-other/src/main/java/com/coderpwh/controller/OtherController.java
@@ -0,0 +1,36 @@
+package com.coderpwh.controller;
+
+
+import cn.hutool.json.JSONUtil;
+import com.coderpwh.domain.UserDTO;
+import com.coderpwh.holder.LoginUserHolder;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Slf4j
+@RestController
+@RequestMapping(value = "/other")
+public class OtherController {
+
+
+    @Autowired
+    private LoginUserHolder loginUserHolder;
+
+    @RequestMapping(value = "/into", method = RequestMethod.GET)
+    public Map<String, Object> getOtherInfo(String str) {
+        UserDTO userDTO = loginUserHolder.getCurrentUser();
+        log.info("user信息为:{}", JSONUtil.toJsonStr(userDTO));
+
+        Map<String,Object> map = new HashMap<>();
+        map.put("id",userDTO.getId());
+        map.put("str",str);
+        return  map;
+    }
+
+}
