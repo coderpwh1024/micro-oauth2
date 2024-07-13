4c52ca8915c40a006eab4456a435a1c35da36e6d
pengwenhao <pengwenhao@aliyun.com>
2024-07-13 14:21:16 +0800

    fix:update loginUserHolder

--- micro-oauth2-api/src/main/java/com/coderpwh/client/OtherProviderClient.java
+++ micro-oauth2-api/src/main/java/com/coderpwh/client/OtherProviderClient.java
@@ -1,12 +1,13 @@
 package com.coderpwh.client;
 
+import com.coderpwh.config.FeignHeadersInterceptor;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Map;
 
-@FeignClient(value = "authOther")
+@FeignClient(value = "authOther",configuration = FeignHeadersInterceptor.class)
 public interface OtherProviderClient {
 
     @GetMapping("/other/info")
--- micro-oauth2-other/src/main/java/com/coderpwh/holder/LoginUserHolder.java
+++ micro-oauth2-other/src/main/java/com/coderpwh/holder/LoginUserHolder.java
@@ -2,6 +2,8 @@ package com.coderpwh.holder;
 
 import javax.servlet.http.HttpServletRequest;
 
+import cn.hutool.json.JSONUtil;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.request.RequestContextHolder;
@@ -11,11 +13,12 @@ import com.coderpwh.domain.UserDTO;
 import org.springframework.web.server.ServerWebExchange;
 import cn.hutool.core.convert.Convert;
 import cn.hutool.json.JSONObject;
-
+import com.nimbusds.jose.*;
 
 /**
  * @author coderpwh
  */
+@Slf4j
 @Component
 public class LoginUserHolder {
 
@@ -25,14 +28,26 @@ public class LoginUserHolder {
         ServletRequestAttributes servletRequestAttributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
         HttpServletRequest request = servletRequestAttributes.getRequest();
 
-        String userStr = request.getHeader("user");
-        JSONObject userJsonObject = new JSONObject(userStr);
+        try {
+            String authorization = request.getHeader("Authorization");
+            log.info("Authorization:{}",authorization);
 
-        UserDTO userDTO = new UserDTO();
-        userDTO.setPassword(userJsonObject.getStr("user_name"));
-        userDTO.setId(Convert.toLong(userJsonObject.get("id")));
-        userDTO.setRoles(Convert.toList(String.class, userJsonObject.get("authorities")));
-        return userDTO;
+            String realToken = authorization.replace("Bearer ", "");
+            JWSObject jwsObject = JWSObject.parse(realToken);
+            String userStr = jwsObject.getPayload().toString();
+
+//        String userStr = request.getHeader("user");
+            JSONObject userJsonObject = new JSONObject(userStr);
+
+            UserDTO userDTO = new UserDTO();
+            userDTO.setPassword(userJsonObject.getStr("user_name"));
+            userDTO.setId(Convert.toLong(userJsonObject.get("id")));
+            userDTO.setRoles(Convert.toList(String.class, userJsonObject.get("authorities")));
+            return userDTO;
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return  null;
     }
 
 }
